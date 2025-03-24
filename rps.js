f292ea9158f6e325758f02e526dabd28cbb7689e
tim48-robot <justintimothy303@gmail.com>
2025-03-24 12:59:22 +0700

    Updated Draft 2, With Button but no UIUX yet

--- rps.js
+++ rps.js
@@ -1,56 +1,54 @@
-function getRandomChoice(){
-    let choiceslength = ["gunting", "kertas", "batu"]
-    let choiceindex = Math.floor(Math.random()*choiceslength.length)
-    let computerchoice = choiceslength[choiceindex]
-    return computerchoice
-}
-
-const tabelKemenangan = {
-    batu:"gunting",
-    gunting:"kertas",
-    kertas:"batu"
-}
-
-let looping = true;
-while (looping) {
-let userinput = prompt("Silahkan pilih antara gunting batu dan kertas: ").toLowerCase()
-let computerchoice = getRandomChoice();
-if (tabelKemenangan[userinput]==computerchoice){
-    let input2 = prompt("Kamu menang! Apakah kamu ingin bermain lagi? [Y/N]")
-    if (input2 == 'N'){
-        break;
-    } if (input2 == null) {
-        break;
+    function getRandomChoice(){
+        let choiceslength = ["gunting", "kertas", "batu"]
+        let choiceindex = Math.floor(Math.random()*choiceslength.length)
+        let computerchoice = choiceslength[choiceindex]
+        return computerchoice
     }
-    else{
-        continue;
-    }
-}
-if (tabelKemenangan[userinput]!=computerchoice){
-    let input2 = prompt("Kamu kalah! Apakah kamu ingin bermain lagi? [Y/N]").toUpperCase();
-    if (input2 == 'N'){
-        break;
-    } if (input2 == null) {
-        break;
-    }
-    else{
-    }
-}
 
-if(userinput == computerchoice){
-    let input2 = prompt("Seri! Apakah kamu ingin bermain lagi? [Y/N]").toUpperCase();
-    if (input2 == 'N'){
-        break;
-    }if (input2 == null) {
-        break;
+    const tabelKemenangan = {
+        batu:"gunting",
+        gunting:"kertas",
+        kertas:"batu"
     }
-    else{
+
+    let userinput = ""
+
+    const button1 = document.querySelectorAll("button")
+    button1.forEach(button => {
+    button.addEventListener("click", (event) => {
+
+
+    const container = document.getElementsByTagName('h2');
+    while (container.length > 0) {
+        container[0].remove();
     }
-}
 
-else {
-    console.log("LANJUT")
-}
 
-}
+    userinput = event.target.className;
+    let computerchoice = getRandomChoice();
+    const h11 = document.querySelector("body")
+    const h21 = document.createElement("h2")
+    const h22 = document.createElement("h2")
+    const h23 = document.createElement("h2")
+    h23.textContent = "Komputer memilih " + computerchoice
+    h22.textContent= "Anda memilih " + userinput
+    h21.textContent = ""
+    h11.appendChild(h22);
+    h11.appendChild(h23);
+    h11.appendChild(h21);
 
+    if (tabelKemenangan[userinput]==computerchoice){
+        h21.textContent="Anda Menang"
+    }
+
+    else if(userinput == computerchoice){
+        h21.textContent="Anda seri"
+    }
+
+    else if (tabelKemenangan[userinput]!=computerchoice){
+        h21.textContent="Anda kalah"
+    }
+
+
+})
+})  
